8024ab6da67b0458b210b022f6c48ddb068c4d20
cpsajin007 <50776585+cpsajin007@users.noreply.github.com>
2021-10-17 14:13:00 +0530

    Create task.py

---
+++ task.py
@@ -0,0 +1,50 @@
+import os
+import sys
+from typing import Collection
+from pyspark import SparkConf,SparkContext
+from pyspark import sql
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as sf
+from pyspark.sql.functions import col, column, explode
+import pyspark
+spark = SparkSession.builder \
+        .master("local") \
+        .appName("App Name") \
+        .getOrCreate() 
+
+
+        
+df=spark.read.option("multiline","true").json("file:///pysparkdemo/demonew.json")
+# df.show()
+# details = df.withColumn("city",sf.explode(sf.col("data.results.user.location.city")))
+# details.show()
+# street = df.withColumn("street", sf.explode(sf.col("data.results.user.location.street")))
+# street.show()
+df.show()
+
+
+# original code
+location= df.select(explode("data.results.user.location").alias("location"))
+location.show()
+
+x= location.select(col("location.city").alias("city"),
+col("location.street").alias("street"),
+col("location.state").alias("state"))
+x.show()
+x.filter("location.city LIKE 'lincoln'").show()
+
+# df2=df.select(col("data.results.user.location.city"),
+# col("data.results.user.location.street"),
+# col("data.results.user.location.state")
+# ).show(truncate=False)
+
+
+
+
+
+
+# sajin = df.withColumn("street", sf.explode(sf.col("data.results.user.location.street"))) \
+#   .withColumn("City", sf.explode(sf.col("data.results.user.location.city"))) \
+#   .withColumn("state",sf.explode(sf.col("data.results.user.location.state"))) \
+#   .distinct()
+# sajin.show()
